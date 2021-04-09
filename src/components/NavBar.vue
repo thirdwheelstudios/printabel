84f31a69567fb04e723dcda7e0f453d907719a8b
Phil Crossland <phil@thirdwheelstudios.com>
2021-04-09 22:04:00 +0100

    Tweaked navbar and routing

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,20 +1,27 @@
 <template>
   <nav>
     <router-link to="/" class="brand">Printabel</router-link>
+    <ul>
+      <li>
+        <router-link to="/create">Create Labels</router-link>
+      </li>
+    </ul>
   </nav>
 </template>
 
 <style scoped>
 nav {
-  padding: 1em;
-  background: linear-gradient(to right, #303f9f, #3f51b5);
+  display: flex;
+  background: linear-gradient(to right, #303f9f 66%, #3f51b5);
 }
 
 a {
   color: rgba(255, 255, 255, 0.75);
   text-decoration: none;
   padding: 0.25em 0.5em;
+  margin: 0.25em;
   border-radius: 1.5em;
+  font-weight: 700;
 }
 
 a:hover {
@@ -23,7 +30,13 @@ a:hover {
 }
 
 .brand {
-  font-weight: 700;
   font-size: 1.5em;
+}
+
+ul {
+  display: flex;
+  list-style: none;
+  padding-left: 0;
+  text-transform: uppercase;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,20 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import CreateLabels from "../views/CreateLabels.vue";
+import Home from '@/views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "CreateLabels",
-    component: CreateLabels,
+    name: "Home",
+    component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/create",
+    name: "CreateLabels",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ "../views/CreateLabels.vue"),
   },
 ];
 
