e03946ef0768a32b386859412dcc17bffa77655b
Phil Crossland <phil@thirdwheelstudios.com>
2021-04-09 18:45:28 +0100

    Initial nav bar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <router-link to="/" class="brand">Printabel</router-link>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  padding: 1em;
+  background: linear-gradient(to right, #303f9f, #3f51b5);
+}
+
+a {
+  color: rgba(255, 255, 255, 0.85);
+  text-decoration: none;
+}
+
+.brand {
+  font-weight: 700;
+  font-size: 1.5em;
+}
+</style>
---
+++ src/views/CreateLabels.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Create Labels</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'CreateLabels',
+})
+</script>
+
+<style scoped>
+
+</style>
